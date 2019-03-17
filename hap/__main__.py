6fa76e0b1fa93e453069c3662e5cbecf3c623a57
Prashant Sinha <prashant+git@noop.pw>
2019-03-17 22:42:07 +0100

    Move to new hap api

--- hap/__main__.py
+++ hap/__main__.py
@@ -2,10 +2,12 @@ import signal
 
 from pyhap.accessory_driver import AccessoryDriver
 
-from .bridge import bridge
+from .bridge import init_bridge
 from . import config
 
-driver = AccessoryDriver(bridge, port=51826, persist_file=config.persist_file)
+
+driver = AccessoryDriver(port=51826, persist_file=config.persist_file)
+bridge = init_bridge(driver)
 
 signal.signal(signal.SIGINT, driver.signal_handler)
 signal.signal(signal.SIGTERM, driver.signal_handler)
--- hap/bridge.py
+++ hap/bridge.py
@@ -6,12 +6,13 @@ from .accessories import TemperatureSensor, HumiditySensor
 
 logging.basicConfig(level=logging.INFO)
 
-
-bridge = Bridge(display_name='Krypton Bridge')
-bridge.add_accessory(TemperatureSensor('Temperature'))
-bridge.add_accessory(HumiditySensor('Humidity'))
-bridge.set_info_service(
-  firmware_revision='v1.5',
-  manufacturer='noop.pw',
-  model='pw.noop.hap.bridge',
-  serial_number='42-BR-DORM')
+def init_bridge(driver):
+  bridge = Bridge(driver, display_name='Krypton Bridge')
+  bridge.add_accessory(TemperatureSensor('Temperature'))
+  bridge.add_accessory(HumiditySensor('Humidity'))
+  bridge.set_info_service(
+    firmware_revision='v1.5',
+    manufacturer='noop.pw',
+    model='pw.noop.hap.bridge',
+    serial_number='42-BR-DORM')
+  return bridge
