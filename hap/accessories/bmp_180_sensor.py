1d552de5fe856e654cc91c21113259da92d2ea0d
Prashant Sinha <prashant+git@noop.pw>
2019-03-17 22:36:01 +0100

    fix new accessory import api

--- hap/accessories/bmp_180_sensor.py
+++ hap/accessories/bmp_180_sensor.py
@@ -1,4 +1,4 @@
-from pyhap.accessory import AsyncAccessory
+from pyhap.accessory import Accessory
 from pyhap.const import CATEGORY_SENSOR
 
 from bme280 import bme280
@@ -8,7 +8,7 @@ bme280.bme280_i2c.set_default_bus(1)
 bme280.setup()
 
 
-class TemperatureSensor(AsyncAccessory):
+class TemperatureSensor(Accessory):
   category = CATEGORY_SENSOR
 
   def __init__(self, *args, **kwargs):
@@ -23,12 +23,12 @@ class TemperatureSensor(AsyncAccessory):
     service = self.add_preload_service('TemperatureSensor')
     self.temperature = service.configure_char('CurrentTemperature')
 
-  @AsyncAccessory.run_at_interval(5)
+  @Accessory.run_at_interval(5)
   async def run(self):
     self.temperature.set_value(bme280.read_temperature())
 
 
-class HumiditySensor(AsyncAccessory):
+class HumiditySensor(Accessory):
   category = CATEGORY_SENSOR
 
   def __init__(self, *args, **kwargs):
@@ -43,6 +43,6 @@ class HumiditySensor(AsyncAccessory):
     service = self.add_preload_service('HumiditySensor')
     self.humidity = service.configure_char('CurrentRelativeHumidity')
 
-  @AsyncAccessory.run_at_interval(5)
+  @Accessory.run_at_interval(5)
   async def run(self):
     self.humidity.set_value(bme280.read_humidity())
